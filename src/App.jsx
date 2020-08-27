1da7aae934189ee58b7630585c23be112ac59684
VitorRibeiroCustodio <vitorribeiro221196@gmail.com>
2020-08-27 17:43:58 -0300

    completed editor

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,14 @@ import 'draft-js-emoji-plugin/lib/plugin.css';
 import './App.css';
 import CustomComponent from './CustomComponent.jsx';
 
+const inlineToolbarPlugin = createInlineToolbarPlugin();
+const { InlineToolbar } = inlineToolbarPlugin;
+
+const emojiPlugin = createEmojiPlugin();
+const { EmojiSelect } = emojiPlugin;
+
+const plugins = [inlineToolbarPlugin, emojiPlugin];
+
 const componentType = 'componentType';
 const novaticsColor = '#513BFF';
 
@@ -24,36 +32,71 @@ const getEntityStrategy = (entityType) => (
   }, callback);
 };
 
+const COMPOSITE_DECORATOR = new CompositeDecorator([
+  {
+    strategy: getEntityStrategy(componentType),
+    component: CustomComponent,
+  },
+]);
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorState: EditorState.createEmpty(),
+      editorState: EditorState.createEmpty(COMPOSITE_DECORATOR),
       htmlText: '',
     };
-    this.onChange = editorState => this.setState({ editorState });
   }
 
-  addCustomComponent = () => {}
+  updateEditorState = (newEditorState) => {
+    this.setState({ editorState: EditorState.set(newEditorState, {
+      decorator: COMPOSITE_DECORATOR
+    })})
+  }
 
-  convertToHtml = () => { }
+  addCustomComponent = () => {
+    const { editorState } = this.state;
+    const contentState = editorState.getCurrentContent();
+    const selectionState = editorState.getSelection();
+
+    let newContentState = contentState.createEntity(componentType, "MUTABLE", {
+      color: novaticsColor,
+      componentName: 'Novatics',
+    });
+    const entityKey = contentState.getLastCreatedEntityKey();
+    newContentState = Modifier.applyEntity(newContentState, selectionState, entityKey);
+    const newEditorState = EditorState.push(editorState, newContentState, 'apply-entity');
+    this.setState({ editorState: EditorState.moveFocusToEnd(newEditorState) });
+  }
+
+  convertToHtml = (editorState) => {
+    const convertedToHtml = HTMLConverter.convertToHTML({
+      entityToHTML: (entity, originalText) => {
+        if (entity.type === componentType) {
+          return `<${entity.data.componentName}>${originalText}</${entity.data.componentName}>`
+        }
+      },
+    })(editorState.getCurrentContent());
+
+    this.setState({ htmlText: convertedToHtml });
+   }
 
   render() {
     return (
       <div className="appWrapper">
         <div className="wrapper">
-          {/* <button onClick={this.addCustomComponent}>Adicionar Componente React</button> */}
-          {/* <button onClick={() => this.convertToHtml(this.state.editorState)}>Formato HTML</button> */}
-          {/* <EmojiSelect /> */}
+          <button onClick={this.addCustomComponent}>Adicionar Componente React</button>
+          <button onClick={() => this.convertToHtml(this.state.editorState)}>Formato HTML</button>
+          <EmojiSelect />
         </div>
         <div className="editorContainer">
           <Editor
             editorState={this.state.editorState}
-            onChange={this.onChange}
+            onChange={this.updateEditorState}
             placeholder="Editor"
+            plugins={plugins}
           />
-          {/* <InlineToolbar /> */}
+          <InlineToolbar />
         </div>
         <div className="wrapper">
           {this.state.htmlText}
